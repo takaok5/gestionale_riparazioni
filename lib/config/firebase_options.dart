6a8e3cc41be97d99a19d76e798fe51e996a3ad95
takaok5 <143890063+takaok5@users.noreply.github.com>
2025-02-09 20:45:36 +0100

    Update firebase_options.dart

--- lib/config/firebase_options.dart
+++ lib/config/firebase_options.dart
@@ -12,6 +12,8 @@ class DefaultFirebaseOptions {
         return android;
       case TargetPlatform.iOS:
         return ios;
+      case TargetPlatform.windows: // Aggiunto supporto Windows
+        return web; // Usa le stesse configurazioni del web per Windows
       default:
         throw UnsupportedError(
           'DefaultFirebaseOptions are not supported for this platform.',
@@ -19,31 +21,30 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // Questi valori dovrebbero essere sostituiti con quelli reali dal Firebase Console
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: const String.fromEnvironment('FIREBASE_WEB_API_KEY'),
-    appId: const String.fromEnvironment('FIREBASE_WEB_APP_ID'),
-    messagingSenderId: const String.fromEnvironment('FIREBASE_SENDER_ID'),
-    projectId: const String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    authDomain: const String.fromEnvironment('FIREBASE_AUTH_DOMAIN'),
-    storageBucket: const String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
+  static final FirebaseOptions web = FirebaseOptions( // Rimosso const ridondante
+    apiKey: String.fromEnvironment('AIzaSyBa0HMvzctZ-Xt-z2uEFxJelBtcQEcS-Zc'),
+    appId: String.fromEnvironment('1:16059806186:web:b20864941d6a46dbbf4293'),
+    messagingSenderId: String.fromEnvironment('16059806186'),
+    projectId: String.fromEnvironment('gestionale-riparazioni'),
+    authDomain: String.fromEnvironment('gestionale-riparazioni.firebaseapp.com'),
+    storageBucket: String.fromEnvironment('gestionale-riparazioni.firebasestorage.app'),
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: const String.fromEnvironment('FIREBASE_ANDROID_API_KEY'),
-    appId: const String.fromEnvironment('FIREBASE_ANDROID_APP_ID'),
-    messagingSenderId: const String.fromEnvironment('FIREBASE_SENDER_ID'),
-    projectId: const String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    storageBucket: const String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
+  static final FirebaseOptions android = FirebaseOptions( // Rimosso const ridondante
+    apiKey: String.fromEnvironment('FIREBASE_ANDROID_API_KEY'),
+    appId: String.fromEnvironment('FIREBASE_ANDROID_APP_ID'),
+    messagingSenderId: String.fromEnvironment('FIREBASE_SENDER_ID'),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: const String.fromEnvironment('FIREBASE_IOS_API_KEY'),
-    appId: const String.fromEnvironment('FIREBASE_IOS_APP_ID'),
-    messagingSenderId: const String.fromEnvironment('FIREBASE_SENDER_ID'),
-    projectId: const String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    storageBucket: const String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
-    iosClientId: const String.fromEnvironment('FIREBASE_IOS_CLIENT_ID'),
-    iosBundleId: const String.fromEnvironment('FIREBASE_IOS_BUNDLE_ID'),
+  static final FirebaseOptions ios = FirebaseOptions( // Rimosso const ridondante
+    apiKey: String.fromEnvironment('FIREBASE_IOS_API_KEY'),
+    appId: String.fromEnvironment('FIREBASE_IOS_APP_ID'),
+    messagingSenderId: String.fromEnvironment('FIREBASE_SENDER_ID'),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
+    iosClientId: String.fromEnvironment('FIREBASE_IOS_CLIENT_ID'),
+    iosBundleId: String.fromEnvironment('FIREBASE_IOS_BUNDLE_ID'),
   );
 }
